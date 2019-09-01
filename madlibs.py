b6d586efb5caa1493c01098b04e2490a566b874d
Luke <deandrevidal@aol.com>
2019-09-01 13:11:05 -0700

    start madlibs homework

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+
+color = input("Enter a color: ")
+pluralNoun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("His eyes were an odd shade of " + color + ".")
+print(pluralNoun + " are blue.")
+print("I love " + celebrity)
