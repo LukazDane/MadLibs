fb7f12df44ba99deb4a5e8a7be363d45c5bc8d9f
Luke <deandrevidal@aol.com>
2019-09-02 11:17:27 -0700

    create empty story

--- app.py
+++ app.py
@@ -1,29 +1,24 @@
-# import the Flask class from the flask module
-from flask import Flask, render_template, request
+# Attempt 3 - Starting over because I was doing way to much.
 
-# create the application object
-app = Flask(__name__)
+# Create a list of words [required]
+words_list = list()
 
-# use decorators to link the function to a url
-@app.route('/')
-def home():
-    return render_template("MadLib.html")  # render request template
+# append to list
 
 
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+def create(item):
+    words_list.append(item)
+
+# User Input Functions
 
 
-@app.route('/result', methods=['POST'])
-def result():
-    if request.method == 'POST':
-        result = request.form
-        return render_template("result.html", result=result)  # render results
+def user_input(prompt):
+    user_input = input(prompt)
+    return user_input
 
 
-# start the server with the 'run()' method
-if __name__ == '__main__':
-    app.run(debug=True)
+def madLibs():
+    # Start the game
+    print("You awake in a dimly lit room. As you look around you notice it appears to be a waiting room of sorts. There are no doors or windows as far as you can tell. You feel nothing but a stiff cold breeze that appears to be coming from all directions. The corners of the room seem darker than the rest. Alost as if the walls are ovrlapping eachother and folding into a space behind themselves. Where am I? ")
+    print("A voice behiind you speaks, soft but heavy,\" ")
+    # Display story with blanks
