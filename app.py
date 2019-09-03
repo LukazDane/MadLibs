fcd9f1546ee9be0dee75e82c26b257fa9a0f4ebe
Luke <deandrevidal@aol.com>
2019-09-03 11:06:32 -0700

    removed old story

--- app.py
+++ app.py
@@ -32,8 +32,6 @@ def madLibs():
     print("A voice behiind you speaks, soft but heavy...\n \"Hello again, You've been here quite a while........ \n haven't you?\"")
 
 
-# Display story with blanks
-# print("You've been here quite a while, haven't you. You poor ___ creature. Your soul is as ___ and dry as the bark of the Yggdrasil. I know how you fear the unkown. Don't fret, it's nowhere near as ___ and dark as you believe. Somewhere in this room is a key, shaped like a large ___. Touch it. If you are correct you will earn your freedom and another chance. Fail and you will hear a ___ sound, like that of a thousand souls churning in fire and you shall join them. Now __ as fast as you can! If you don't leave this place soon, you may forget how.... but that wouldn't be so bad would it?")
 # Ask user to input words to fill in blanks
 firstAdj = user_input("Give me an adjective: ")
 # Everytime the user gives you a word append that word to the list by calling the create function
